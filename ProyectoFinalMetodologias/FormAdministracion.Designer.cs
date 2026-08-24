b3b933d7bd2803d8e2d4bf75044e1ef558bcd0ab
angel david <ezzeangelsent@gmail.com>
2026-08-23 22:11:19 -0600

    codigo completado y corrido

---
+++ ProyectoFinalMetodologias/FormAdministracion.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ProyectoFinalMetodologias
+{
+    partial class FormAdministracion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(767, 236);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.DecimalPlaces = 2;
+            numericUpDown1.Location = new Point(601, 304);
+            numericUpDown1.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(601, 271);
+            label1.Name = "label1";
+            label1.Size = new Size(145, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Selecciona una habitación";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(601, 348);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Actualizar Precio";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // FormAdministracion
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(dataGridView1);
+            Name = "FormAdministracion";
+            Text = "FormAdministracion";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ ProyectoFinalMetodologias/FormAdministracion.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ProyectoFinalMetodologias
+{
+    // Clase auxiliar para la vista de administración
+    public class HabitacionVista
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+        public decimal Precio { get; set; }
+    }
+
+    public partial class FormAdministracion : Form
+    {
+        private List<HabitacionVista> listaHabitaciones = new List<HabitacionVista>();
+
+        public FormAdministracion()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAdministracion_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+        }
+
+        private void CargarDatos()
+        {
+            listaHabitaciones = new List<HabitacionVista>
+            {
+                new HabitacionVista { Id = 1, Tipo = "Sencilla", Precio = 500.00m },
+                new HabitacionVista { Id = 2, Tipo = "Doble", Precio = 850.00m },
+                new HabitacionVista { Id = 3, Tipo = "Suite", Precio = 1500.00m }
+            };
+
+            RefrescarGrid();
+        }
+
+        private void RefrescarGrid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = listaHabitaciones;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && dataGridView1.CurrentRow != null)
+            {
+                string tipo = dataGridView1.CurrentRow.Cells["Tipo"].Value.ToString();
+                decimal precio = Convert.ToDecimal(dataGridView1.CurrentRow.Cells["Precio"].Value);
+
+                label1.Text = "Modificando: " + tipo;
+                numericUpDown1.Value = precio;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int idSeleccionado = Convert.ToInt32(dataGridView1.CurrentRow.Cells["Id"].Value);
+                var habitacion = listaHabitaciones.FirstOrDefault(h => h.Id == idSeleccionado);
+
+                if (habitacion != null)
+                {
+                    habitacion.Precio = numericUpDown1.Value;
+                    RefrescarGrid();
+                    MessageBox.Show("¡Precio actualizado correctamente!", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor selecciona una habitación de la lista.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
